5fcc382c84f065910b892fe2bfabb1dfe9d31b99
Asuka <asuka@angelwork.org>
2024-05-22 01:12:37 +0800

    Append page_count to return of SelectPage of schema

--- schema.select.go
+++ schema.select.go
@@ -14,7 +14,7 @@ func (sc *Schema[T]) Select(ctx context.Context, where string, args ...any) ([]*
 
 // SelectPage selects a page of records from the database.
 // page_idx shoud be 1-based.
-// Return: records, current page index, page size, total count, error
-func (sc *Schema[T]) SelectPage(ctx context.Context, page_idx, page_size int64, where string, args ...any) ([]*T, int64, int64, int64, error) {
+// Return: records, current page index, page size, page count, total count, error
+func (sc *Schema[T]) SelectPage(ctx context.Context, page_idx, page_size int64, where string, args ...any) ([]*T, int64, int64, int64, int64, error) {
 	return sc.entity.SelectPage(ctx, page_idx, page_size, where, args...)
 }
