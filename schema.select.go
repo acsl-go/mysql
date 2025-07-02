c0a9a5c718ea42ebcf85bd5354e527da104abf42
Angel-Asuka <asuka@angelwork.org>
2025-07-02 11:44:53 +0000

    Append Tx support on schema.selelct*

--- schema.select.go
+++ schema.select.go
@@ -18,3 +18,18 @@ func (sc *Schema[T]) Select(ctx context.Context, where string, args ...any) ([]*
 func (sc *Schema[T]) SelectPage(ctx context.Context, page_idx, page_size int64, where string, args ...any) ([]*T, int64, int64, int64, int64, error) {
 	return sc.entity.SelectPage(ctx, page_idx, page_size, where, args...)
 }
+
+func (sc *Schema[T]) SelectOneEx(ctx context.Context, db IDBLike, where string, args ...any) (*T, error) {
+	return sc.entity.SelectOneEx(ctx, db, where, args...)
+}
+
+func (sc *Schema[T]) SelectEx(ctx context.Context, db IDBLike, where string, args ...any) ([]*T, error) {
+	return sc.entity.SelectEx(ctx, db, where, args...)
+}
+
+// SelectPage selects a page of records from the database.
+// page_idx shoud be 1-based.
+// Return: records, current page index, page size, page count, total count, error
+func (sc *Schema[T]) SelectPageEx(ctx context.Context, db IDBLike, page_idx, page_size int64, where string, args ...any) ([]*T, int64, int64, int64, int64, error) {
+	return sc.entity.SelectPageEx(ctx, db, page_idx, page_size, where, args...)
+}
